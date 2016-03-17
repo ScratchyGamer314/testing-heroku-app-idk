4dc50ee956bc4f442946aa348816020432953d87
bhupendra1011 <bhupendra1011@gmail.com>
2016-03-17 19:19:08 +0530

    created ui for chat

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,22 @@
  var socket = io();
  // listen for server connection
- socket.on("connect",function(){
+ socket.on("connect", function() {
    console.log("Connected to Socket I/O Server!");
  });
 
  //setup for custom events
- socket.on("message",function(message){
+ socket.on("message", function(message) {
    console.log("New Message !");
    console.log(message.text);
+ });
+
+ // handles submitting of new message
+ var $form = $("#messageForm");
+ var $message = $form.find('input[name=message]');
+ $form.on("submit", function(event) {
+   event.preventDefault();
+   socket.emit("message", {
+     text: $message.val()
+   });
+   $message.val('');
  });
