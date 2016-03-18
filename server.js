db39987622e73dfeb41f2e50a8bfa3a3d9923244
bhupendra1011 <bhupendra1011@gmail.com>
2016-03-18 14:38:41 +0530

    add current users command

--- server.js
+++ server.js
@@ -12,21 +12,52 @@ var clientInfo = {};
 var io = require("socket.io")(http);
 
 // expose the folder via express thought
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + '/public'));
+
+// send current users to provided scoket
+function sendCurrentUsers(socket)
+{ // loading current users
+  var info = clientInfo[socket.id];
+  var users = [];
+  if (typeof info === 'undefined')
+  {
+    return;
+  }
+  // filte name based on rooms
+  Object.keys(clientInfo).forEach(function(socketId) {
+    var userinfo  = clientInfo[socketId];
+    // check if user room and selcted room same or not
+    // as user should see names in only his chat room
+    if (info.room == userinfo.room)
+    {
+      users.push(userinfo.name);
+    }
+
+  });
+   // emit message when all users list
+
+  socket.emit("message",{
+    name : "System",
+    text : "Current Users : "+users.join(', '),
+    timestamp : moment().valueOf()
+  });
+
+}
+
 
 // io.on listens for events
-io.on("connection",function(socket){
+io.on("connection", function(socket) {
   console.log("User is connected");
 
   //for disconnection
-  socket.on("disconnect" , function (){
+  socket.on("disconnect", function() {
     var userdata = clientInfo[socket.id];
     if (typeof(userdata !== undefined)) {
       socket.leave(userdata.room); // leave the room
       //broadcast leave room
       socket.broadcast.to(userdata.room).emit("message", {
         text: userdata.name + " has left",
-        name:"System",
+        name: "System",
         timestamp: moment().valueOf()
       });
 
@@ -37,35 +68,40 @@ io.on("connection",function(socket){
   });
 
   // for private chat
-  socket.on('joinRoom',function(req){
+  socket.on('joinRoom', function(req) {
     clientInfo[socket.id] = req;
     socket.join(req.room);
     //broadcast new user joined room
-    socket.broadcast.to(req.room).emit("message",{
-      name:"System",
-      text:req.name + ' has joined',
-      timestamp:moment().valueOf()
+    socket.broadcast.to(req.room).emit("message", {
+      name: "System",
+      text: req.name + ' has joined',
+      timestamp: moment().valueOf()
     });
 
   });
 
-  socket.emit("message",{
-    text:"Welcome to Chat Appliction !",
+  socket.emit("message", {
+    text: "Welcome to Chat Appliction !",
     timestamp: moment().valueOf(),
-    name:"System"
+    name: "System"
   });
 
   // listen for client message
-  socket.on("message",function (message) {
-     console.log("Message Received : " + message.text);
-     //broadcast to all users except for sender
-     message.timestamp = moment().valueOf();
-     //socket.broadcast.emit("message",message);
-     // now message should be only sent to users who are in same room
-     socket.broadcast.to(clientInfo[socket.id].room).emit("message",message);
+  socket.on("message", function(message) {
+    console.log("Message Received : " + message.text);
+    // to show all current users
+    if (message.text === "@currentUsers") {
+      sendCurrentUsers(socket);
+    } else {
+      //broadcast to all users except for sender
+      message.timestamp = moment().valueOf();
+      //socket.broadcast.emit("message",message);
+      // now message should be only sent to users who are in same room
+      socket.broadcast.to(clientInfo[socket.id].room).emit("message", message);
+    }
 
   });
 });
-http.listen(PORT,function(){
+http.listen(PORT, function() {
   console.log("server started");
 });
